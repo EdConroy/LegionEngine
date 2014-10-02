de52ec0f3159b4913bc46016d3afb4fe66ae959a
Ed Conroy <epc5@njit.edu>
2014-10-02 10:24:31 -0400

    Create background.c

---
+++ src/background.c
@@ -0,0 +1,44 @@
+#include "background.h"
+
+struct
+{
+	int x;
+	int y;
+	int w;
+	int h;
+	int last_x;
+	int last_y;
+}F_Bck;
+
+void InitBck()
+{
+	F_Bck.x=0;
+	F_Bck.y=0;
+	F_Bck.w=0;
+	F_Bck.h=0;
+	F_Bck.last_x=0;
+	F_Bck.last_y=0;
+}
+
+void Parallax(Sprite* fst, Sprite* scd, SDL_Surface* screen, int x, int y)
+{
+	for(int i = 0; i < 40; ++i)
+	{
+		DrawSprite(fst,screen,F_Bck.x+32, 510,0);
+		F_Bck.last_x = F_Bck.x; 
+		F_Bck.x = F_Bck.last_x + 32;
+	}
+	for(int j = 0; j < 80; ++j)
+	{
+		DrawSprite(scd,screen,F_Bck.x + 64, 610, 0);
+		F_Bck.last_x = F_Bck.x; 
+		F_Bck.x = F_Bck.last_x + 32;
+	}
+}
+void FreeBck(Sprite* fst, Sprite* scd)
+{
+	FreeSprite(fst);
+	FreeSprite(scd);
+	fst = NULL;
+	scd = NULL;
+}
