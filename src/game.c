a20837fb8bd07a46ea030469c9580a26d43eed9c
Ed Conroy <epc5@njit.edu>
2014-10-02 10:31:33 -0400

    Update game.c

--- src/game.c
+++ src/game.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
+#include "glib.h"
 #include "SDL.h"
 #include "SDL_image.h"
 #include "graphics.h"
+#include "fighter.h"
+#include "background.h"
 
 extern SDL_Surface *screen;
 extern SDL_Surface *buffer; /*pointer to the draw buffer*/
@@ -17,6 +20,8 @@ int main(int argc, char *argv[])
   SDL_Surface *temp;
   SDL_Surface *bg;
   Sprite *tile;
+  Fighter* f;
+  Fighter* f2;
   int done;
   int keyn;
   int i;
@@ -30,39 +35,62 @@ int main(int argc, char *argv[])
   if(bg != NULL)
     SDL_BlitSurface(bg,NULL,buffer,NULL);
   tile = LoadSprite("images/32_32_16_2sprite.png",32,32);
+  /*
   if(tile != NULL)for(i = 0;i < 12;i++)
   {
     DrawSprite(tile,buffer,i * tile->w,0,0);
   }
+  */
   done = 0;
+  f = getFighter(0);
+  f2 = getFighter(1);
+  InitCombatant(f);
+  InitCombatant2(f2);
   do
   {
-    ResetBuffer ();
-    DrawMouse();
-    NextFrame();
-    SDL_PumpEvents();
-    keys = SDL_GetKeyState(&keyn);
+    ResetBuffer();
+	SDL_PumpEvents();
+	//Parallax(tile,tile,screen,32,32);
+	keys = SDL_GetKeyState(&keyn);
+	FighterController(f,f2,screen,keys);
+	DrawHealthBar(f,screen,0,0);
+	DrawHealthBar(f2,screen,310,0);
+	/*
     if(SDL_GetMouseState(&mx,&my))
     {
       DrawSprite(tile,buffer,(mx /32) * 32,(my /32) * 32,0); 
     }
+	*/
+	if(f->health <= 0 || f2->health <= 0)
+	{
+		done = 1;
+	}
+    //DrawMouse();
+	NextFrame();
+
     if(keys[SDLK_ESCAPE])done = 1;
   }while(!done);
-  exit(0);		/*technically this will end the program, but the compiler likes all functions that can return a value TO return a value*/
+  exit(0); /*technically this will end the program, but the compiler likes all functions that can return a value TO return a value*/
   return 0;
+  FreeFighter(f);
+  FreeFighter(f2);
+  FreeBck(tile,tile);
 }
 
 void CleanUpAll()
 {
   CloseSprites();
+  //CloseFighter();
   /*any other cleanup functions can be added here*/ 
 }
 
 void Init_All()
 {
   Init_Graphics();
-
-  InitMouse();
+  InitBck();
+  InitFighterList();
+  InitFighterSprite();
+  //InitMouse();
   atexit(CleanUpAll);
 }
 
