3fdca8d583b3fd907bf3ff0b0cc1c73c4946ec47
Ed Conroy <epc5@njit.edu>
2014-10-08 20:42:44 -0400

    Update fighter.h

--- src/fighter.h
+++ src/fighter.h
@@ -1,10 +1,28 @@
 #include "graphics.h"
 
 #define MAX_FIGHTERS				2
+
 #define FIGHTERFLAG_BLOCK			0
 #define FIGHTERFLAG_NOBLOCK			1
 #define FIGHTERFLAG_HITH			2
 #define FIGHTERFLAG_HITL			3
+#define FIGHTERFLAG_WALKL			4
+#define FIGHTERFLAG_WALKR			5
+#define FIGHTERFLAG_JUMP			6
+#define FIGHTERFLAG_IDLE			7
+#define FIGHTERFLAG_CROUCH			8
+
+#define ANIMFLAG_BLOCK				0
+#define ANIMFLAG_NOBLOCK			1
+#define ANIMFLAG_LIGHT				2
+#define ANIMFLAG_MED				3
+#define ANIMFLAG_HEV				4
+#define ANIMFLAG_LAUNCH				5
+#define ANIMFLAG_WALKL				6
+#define ANIMFLAG_WALKR				7
+#define ANIMFLAG_JUMP				8
+#define ANIMFLAG_IDLE				9
+#define ANIMFLAG_CROUCH				10
 
 typedef struct Fighter_T
 {
@@ -37,6 +55,7 @@ typedef struct Fighter_T
 	int is_grounded; 
 	int vy;
 	long flags;
+	long anim_flags;
 }Fighter;
 
 void InitFighterSprite();
@@ -47,7 +66,10 @@ void InitFighterList();
 Fighter * getFighter(int player);
 
 void Dash(Fighter* inflictor, SDL_Surface *buffer, Uint8* keys);
-void FighterController(Fighter* inflictor, Fighter* f2, SDL_Surface *buffer, Uint8* keys);
+//void FighterPull(Fighter* f1, Fighter* f2, Uint8* keys);
+//void DrawFighter(Fighter* f1, Fighter* f2, SDL_Surface* buffer);
+//void UpdateFighter(Fighter* f1, Fighter* f2);
+void FighterController(Fighter* f1, Fighter* f2, SDL_Surface *buffer, Uint8* keys);
 void DrawHealthBar(Fighter* f, SDL_Surface *buffer, int x, int y);
 
 
