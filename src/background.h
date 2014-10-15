6bd51f2bcba7bbc19eb941377a81e3fcd16483ed
Ed Conroy <epc5@njit.edu>
2014-10-14 23:34:59 -0400

    Added Comments

--- src/background.h
+++ src/background.h
@@ -1,5 +1,6 @@
 #include "graphics.h"
 
-void InitBck();
-void Parallax(Sprite* fst, Sprite* scd, SDL_Surface* screen, int x, int y);
-void FreeBck(Sprite* fst, Sprite* scd);
+void InitBck(); /* Initializes the backround to default values */
+void Parallax(Sprite* fst, Sprite* scd, SDL_Surface* screen, int x, int y); /* Creates two scrolling horizontal sprites 
+																			traveling at different speeds across the buffer*/
+void FreeBck(Sprite* fst, Sprite* scd); /* Frees the current memory held by the background images */
