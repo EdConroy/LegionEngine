0ec848d3d0442e0ff059e2c6b417478e2e026406
Ed Conroy <epc5@njit.edu>
2014-10-14 23:35:41 -0400

    Added animation

--- src/background.c
+++ src/background.c
@@ -8,6 +8,7 @@ struct
 	int h;
 	int last_x;
 	int last_y;
+	int frame;
 }F_Bck;
 
 void InitBck()
@@ -18,22 +19,27 @@ void InitBck()
 	F_Bck.h=0;
 	F_Bck.last_x=0;
 	F_Bck.last_y=0;
+	F_Bck.frame = 0;
 }
 
 void Parallax(Sprite* fst, Sprite* scd, SDL_Surface* screen, int x, int y)
 {
+	int frame_count;
 	for(int i = 0; i < 40; ++i)
 	{
-		DrawSprite(fst,screen,F_Bck.x+32, 510,0);
+		DrawSprite(fst,screen,F_Bck.x+32, 510,F_Bck.frame);
+		frame_count = 3;
 		F_Bck.last_x = F_Bck.x; 
 		F_Bck.x = F_Bck.last_x + 32;
 	}
 	for(int j = 0; j < 80; ++j)
 	{
-		DrawSprite(scd,screen,F_Bck.x + 64, 610, 2);
+		DrawSprite(scd,screen,F_Bck.x + 40, 610, F_Bck.frame);
+		frame_count = 3;
 		F_Bck.last_x = F_Bck.x; 
 		F_Bck.x = F_Bck.last_x + 32;
 	}
+	F_Bck.frame = (F_Bck.frame + 1) % frame_count;
 }
 void FreeBck(Sprite* fst, Sprite* scd)
 {
