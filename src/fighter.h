e047c0a4a664df464f1b955b6a3c5193939403dd
Ed Conroy <epc5@njit.edu>
2014-10-21 01:17:17 -0400

    Update fighter.h

--- src/fighter.h
+++ src/fighter.h
@@ -1,5 +1,9 @@
 #include "graphics.h"
 
+#define IS_SET(a,b)				(a & b)
+#define SET_FLAG(a,b)			(a |= b)
+#define REMOVE_FLAG(a,b)		(a &= ~b)
+
 #define MAX_FIGHTERS				2
 
 #define FIGHTERFLAG_BLOCK			0
@@ -12,6 +16,13 @@
 #define FIGHTERFLAG_IDLE			7
 #define FIGHTERFLAG_CROUCH			8
 #define FIGHTERFLAG_BLOCKL			9
+
+#define JUMPFLAG_MOVEL				0
+#define JUMPFLAG_MOVER				1
+#define JUMPFLAG_LIGHT				2
+#define JUMPFLAG_MED				3
+#define JUMPFLAG_HEV				4
+#define JUMPFLAG_LAUNCH				5
 
 #define ANIMFLAG_BLOCK				0
 #define ANIMFLAG_NOBLOCK			1
@@ -52,11 +63,11 @@ typedef struct Fighter_T
 	int combo_count;
 	int facing; 
 	int jump_count;
-	int used; 
-	int is_grounded; 
+	int used;  
 	int vy;
 	long flags;
 	long anim_flags;
+	long f_jump;
 }Fighter;
 
 void InitFighterSprite(); /* Sets the default animation state for fighters */
@@ -66,12 +77,14 @@ void InitFighterList(); /* Initializes the fighter list and populates it */
 
 Fighter * getFighter(int player);
 
-void FighterPull(Fighter* f1, Fighter* f2, Uint8* keys); /* Gets the inputs from the user and sets flags based on those
+void FighterPull(Fighter* f1, Uint8* keys); /* Gets the inputs from the user and sets flags based on those
 														 inputs */
-//void FighterPull2(Fighter* f2, Uint8* keys);
+void FighterPull2(Fighter* f2, Uint8* keys);
 void DrawFighter1(Fighter* f1, SDL_Surface* buffer); /* Draws the fighter based on the flags set by FighterPull */
-//void DrawFighter2(Fighter* f2, SDL_Surface* buffer); /* Draws the fighter based on the flags set by FighterPull */
-//void UpdateFighter(Fighter* f1, Fighter* f2); /* Applies logic to to the action based on the fighter's current flag */
+/*
+void DrawFighter2(Fighter* f2, SDL_Surface* buffer); Draws the fighter based on the flags set by FighterPull
+void UpdateFighter(Fighter* f1, Fighter* f2); Applies logic to to the action based on the fighter's current flag 
+*/
 void FighterController1(Fighter* f1, Fighter* f2, SDL_Surface *buffer);
 void FighterController(Fighter* f1, Fighter* f2, SDL_Surface *buffer, Uint8* keys);/* Does all three of the aforementioned methods
 																				   really ineffiecent but functional */
