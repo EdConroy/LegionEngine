a27efea227cee6ddc98a9426925f8583bfdacd73
Ed Conroy <epc5@njit.edu>
2014-10-26 23:02:20 -0400

    Update audio.h

--- src/audio.h
+++ src/audio.h
@@ -1,3 +1,6 @@
+#ifndef _AUDIO_
+#define _AUDIO_
+
 #include "SDL_audio.h"
 #include "SDL_mixer.h"
 
@@ -5,3 +8,5 @@ void InitMusic();
 int LoadMusic(Mix_Music* music);
 void PlayMusic(Mix_Music* music);
 void FreeMusic(Mix_Music* music);
+
+#endif
