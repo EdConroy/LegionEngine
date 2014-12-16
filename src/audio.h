135f826e5223498a08fcb4bb8cc5bc4e6c5c8596
Ed Conroy <epc5@njit.edu>
2014-12-16 17:54:51 -0500

    Added functionality for sound effects

--- src/audio.h
+++ src/audio.h
@@ -3,6 +3,7 @@
 
 #include "SDL_audio.h"
 #include "SDL_mixer.h"
+#include "fighter.h"
 
 #define STAGE_CLOCKTOWER	0
 #define STAGE_SWAMP			1
@@ -10,8 +11,9 @@
 
 void InitMusic();
 int LoadMusic(Mix_Music* music);
+int LoadSFX(Mix_Music* effect, int character);
 void PlayMusic(Mix_Music* music);
-int PlayEffect(int state, int character);
+void PlayEffect(Mix_Music* effect);
 void FreeMusic(Mix_Music* music);
 
 #endif
